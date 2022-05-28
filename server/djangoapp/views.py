b85ab0ac57b4e06cb08d9ee8048933ae810aef76
SamGorr <chenglongyangcly@gmail.com>
2022-05-28 04:49:14 +0000

    admin

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,16 +30,15 @@ def get_contact(request):
         return render(request, 'djangoapp/contact.html', context)
         
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
-
+def login_request(request):
+    return render(request, 'djangoapp/contact.html', context)
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    return render(request, 'djangoapp/contact.html', context)
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    return render(request, 'djangoapp/contact.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
